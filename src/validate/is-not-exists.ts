29f5cd2fd8baa810a99522b7c4d55305bc0a8115
banmashou <agcwhyinuo@sina.com>
2024-01-14 19:56:57 +0800

    自定义验证规则校验唯一性

---
+++ src/validate/is-not-exists.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator'
+
+// 表字段是否唯一
+export function IsNotExists(table: string, fields: string[], validationOptions?: ValidationOptions) {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      name: 'IsNotExists',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [table],
+      options: validationOptions,
+      validator: {
+        async validate(value: any, args: ValidationArguments) {
+          const prisma = new PrismaClient({ log: ['query'] })
+          const res = await prisma.user.findFirst({
+            where: {
+              OR: fields.map((field) => ({ [field]: value })),
+            },
+          })
+          return !Boolean(res)
+        },
+      },
+    })
+  }
+}
